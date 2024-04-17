3e89642626f120def931bbd3874ae073f545bf51
MuhammadaliAliyev <muhammadalialiyev4174@gmail.com>
2024-04-17 08:36:36 +0500

    text to html initial commit

---
+++ convertor/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .forms import EditorForm
+# Create your views here.
+
+def index(request):
+    form = EditorForm()
+    return render(request,'index.html',{'form':form})
